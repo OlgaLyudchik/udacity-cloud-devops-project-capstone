60d9bd74345ed1b7ca27cdee6aee84de1d33e774
Olga <helga.lyudchik@gmail.com>
2021-03-05 13:30:00 +0100

    feat: Configure eslint for node.js package

--- weather-app/server.js
+++ weather-app/server.js
@@ -1,40 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-var path = require('path');
-const app = express()
+const path = require('path');
+const app = express();
 const port = process.env.PORT;
-const apiKey = process.env.API_KEY
+const apiKey = process.env.API_KEY;
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.render('pages/index', {weather: null, error: null});
-})
+    res.render('pages/index', { weather: null, error: null });
+});
 
 app.post('/', function (req, res) {
-  let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    const city = req.body.city;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
-  request(url, function (err, response, body) {
-    if(err){
-      res.render('pages/index', {weather: null, error: 'Error, please try again'});
-    } else {
-      let weather = JSON.parse(body)
-      if(weather.main == undefined){
-        res.render('pages/index', {weather: null, error: 'Error, please try again'});
-      } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('pages/index', {weather: weatherText, error: null});
-      }
-    }
-  });
-})
+    request(url, function (err, response, body) {
+        if (err) {
+            res.render('pages/index', { weather: null, error: 'Error, please try again' });
+        } else {
+            const weather = JSON.parse(body);
+            if (weather.main === undefined) {
+                res.render('pages/index', { weather: null, error: 'Error, please try again' });
+            } else {
+                const weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                res.render('pages/index', { weather: weatherText, error: null });
+            }
+        }
+    });
+});
 
 app.listen(port, function () {
-  console.log(`Weather application is listening on port ${port}...`)
-})
+    console.log(`Weather application is listening on port ${port}...`);
+});
